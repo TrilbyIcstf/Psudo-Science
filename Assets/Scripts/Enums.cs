4c204e8a0e16e80dbf1bab07993455615cfa5a57
TrilbyIcstf <103054155+TrilbyIcstf@users.noreply.github.com>
2022-05-02 13:52:52 -0500

    Whoops
    Forgot to push these

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,28 @@
+public enum Color
+{
+    GHOST,
+    BLACK,
+    BLUE,
+    GREEN,
+    GREY,
+    ORANGE,
+    PINK,
+    PURPLE
+}
+
+public enum Element
+{
+    NULL,
+    WATER,
+    EARTH,
+    FIRE,
+    AIR
+}
+
+public enum AttackType
+{
+    PHYSICAL,
+    MAGICAL,
+    HEALING,
+    STATUS
+}
